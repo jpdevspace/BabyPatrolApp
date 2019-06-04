2eae5f101832ecee5fde4211fcb5a7e54846cfb3
Jean Paul <jjpwebdev@gmail.com>
2019-06-03 22:50:47 -0400

    MongoDB Connection created on the server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,20 @@
+const cors = require('cors');
 const express = require('express');
+const mongoose = require('mongoose');
+
+const configDB = require('./config/configDB');
+
 const app = express();
 const port = 4000;
+
+// Allow cross-origin requests CORS
+app.use(cors());
+
+// Mongoose setup
+mongoose.connect(`mongodb+srv://${configDB.usr}:${configDB.pwd}@babypatrolcluster-uel9p.mongodb.net/test?retryWrites=true&w=majority`, {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection Error:'));
+db.once('open', () => console.log("Connected to DB!"));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
