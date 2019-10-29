e63675d5559289a55c7ff915c62fe4b7855e311e
Jean Paul <jjpwebdev@gmail.com>
2019-10-28 23:26:53 -0400

    Adding Firebase config file

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Initialize Firebase
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "babytimes-9476d.firebaseapp.com",
+  databaseURL: "https://babytimes-9476d.firebaseio.com",
+  projectId: "babytimes-9476d",
+  storageBucket: "babytimes-9476d.appspot.com",
+  messagingSenderId: "702098493706"
+}
+// Initialize Cloud firestore through Firebase 
+firebase.initializeApp(config);
+
+export default firebase;
