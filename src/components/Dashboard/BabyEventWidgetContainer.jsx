c2ab743ab78308e0ce2ec3e93b49baca16588d48
Jean Paul <jjpwebdev@gmail.com>
2020-05-25 11:43:12 -0400

    Fixed CSS

--- src/components/Dashboard/BabyEventWidgetContainer.jsx
+++ src/components/Dashboard/BabyEventWidgetContainer.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const BabyEventWidgetContainer = ({children}) => {
   return (
-    <div id="bp-eventsWidgetContainer">
-        { children }
-    </div>);
+    <div id="bp-bodyWrapper">
+      <div id="bp-eventsWidgetContainer">{children}</div>
+    </div>
+  );
 };
 
 export default BabyEventWidgetContainer;
